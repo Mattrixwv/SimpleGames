661fca7afb2b6b8cca4922e3b19f7af2138efca4
Matthew Ellison <m_ellison@ymail.com>
2018-03-23 13:26:28 -0400

    Created a file for global variables

---
+++ ZombieDice/Headers/DiceVars.hpp
@@ -0,0 +1,16 @@
+//Games/ZombieDice/Headers/DiceVars.hpp
+//Matthew Ellison
+// Created: 3-21-18
+//Modified: 3-21-18
+//This file contains variables needed for the Zombie Dice game
+
+#ifndef DICE_VARS_HPP
+#define DICE_VARS_HPP
+
+
+#define BRAIN 'B'
+#define FEET 'F'
+#define SHOTGUN 'S'
+
+
+#endif //DICE_VARS_HPP
